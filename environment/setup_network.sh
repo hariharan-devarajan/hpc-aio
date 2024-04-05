e697f17d9e77f6f78ccc5af8133147b3fd2b39a7
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-04-05 01:39:45 +0000

    start ssh service

--- environment/setup_network.sh
+++ environment/setup_network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
+service ssh restart
 cp ${SCRIPT_DIR}/hosts /etc/hosts
 ips=`cat ${SCRIPT_DIR}/hosts|awk -F" " '{print $1}'`
 for ssh_ip in $ips;
